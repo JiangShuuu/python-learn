f0824e65f97799173b0ad13a7a68aa82ef20f371
Jiangshuuu <globelex65@gmail.com>
2024-05-26 10:32:30 +0800

    authenticate

--- practice/todoApp/routers/auth.py
+++ practice/todoApp/routers/auth.py
@@ -1,10 +1,22 @@
+from typing import Annotated
 from sqlalchemy.orm import Session
 from fastapi import APIRouter, Depends, HTTPException
 from pydantic import BaseModel
 from models import Users
 from database import get_db
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter()
+bcrypt_context = CryptContext(schemes=['bcrypt'], deprecated='auto')
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(Users).filter(Users.username == username).first()
+    if not user:
+        return False
+    if not bcrypt_context.verify(password, user.hashed_password):
+        return False
+    return True
 
 class CreateUserRequest(BaseModel):
     username: str
@@ -25,10 +37,20 @@ async def create_user(user: CreateUserRequest, db: Session = Depends(get_db)):
         username=user.username,
         first_name=user.first_name,
         last_name=user.last_name,
-        hashed_password=user.password,
+        hashed_password=bcrypt_context.hash(user.password),
         role=user.role
     )
+
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
     return db_user
+
+@router.post('/token')
+async def login_for_access_token(form_data: Annotated[OAuth2PasswordRequestForm, Depends()], db: Session = Depends(get_db)): 
+    
+    user = authenticate_user(form_data.username, form_data.password, db)
+
+    if not user:
+        return 'Failed Authentication'
+    return 'Successfully logged in'
